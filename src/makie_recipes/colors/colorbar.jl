3c48f3a4568667c7b47516bc1afcf3415673c428
Rémi Vezy <VEZY@users.noreply.github.com>
2023-03-19 21:06:42 +0100

    Update colorbar.jl
    Add docs

--- src/makie_recipes/colors/colorbar.jl
+++ src/makie_recipes/colors/colorbar.jl
@@ -3,6 +3,23 @@
 
 Add a colorbar based on the attribute chose to color the plot. plotobject must be a plot of
 an MTG colored by an attribute. Use Makie.Colorbar for any other use case instead.
+
+# Arguments
+
+- `parent`: parent scene
+- `plotobject`: plot object to add the colorbar to
+- `kwargs`: keyword arguments to pass to Makie.Colorbar
+
+# Example
+
+```julia
+using GLMakie, MultiScaleTreeGraph, PlantGeom
+file = joinpath(dirname(dirname(pathof(PlantGeom))), "test", "files", "simple_plant.opf")
+opf = read_opf(file)
+
+f, ax, p = viz(opf, color=:Length)
+colorbar(f[1, 2], p)
+f
 """
 function colorbar(parent, plotobject; kwargs...)
     color = plotobject.attributes.color
