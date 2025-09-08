9d85b4824ae0cc4c13b8b4ca2914cd797b31a545
Rémi Vezy <VEZY@users.noreply.github.com>
2025-09-08 16:46:50 +0200

    Update benchmarks.jl
    disable caching for benchmarks (not in use in this version yet, but will be used in the future when #113 is merged)

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -11,6 +11,7 @@ file_large = "/Users/rvezy/Documents/dev/VPalm_test/tests/test2/test.opf"
 # initialize benchmark suite
 const SUITE = BenchmarkGroup()
 
+const cache = false # Disable caching for benchmarking
 # ---------
 # IO
 # ---------
@@ -31,15 +32,15 @@ opf_medium = read_opf(file_medium)
 opf_large = read_opf(file_large)
 
 # Reference mesh:
-SUITE["OPF plotting"]["single color"]["small file"] = @benchmarkable plantviz($opf_small)
-SUITE["OPF plotting"]["single color"]["medium file"] = @benchmarkable plantviz($opf_medium)
-SUITE["OPF plotting"]["single color"]["large file"] = @benchmarkable plantviz($opf_large)
+SUITE["OPF plotting"]["single color"]["small file"] = @benchmarkable plantviz($opf_small, cache=$cache)
+SUITE["OPF plotting"]["single color"]["medium file"] = @benchmarkable plantviz($opf_medium, cache=$cache)
+SUITE["OPF plotting"]["single color"]["large file"] = @benchmarkable plantviz($opf_large, cache=$cache)
 
 # Single color:
 color = :green
-SUITE["OPF plotting"]["single color"]["small file"] = @benchmarkable plantviz($opf_small, color=$color)
-SUITE["OPF plotting"]["single color"]["medium file"] = @benchmarkable plantviz($opf_medium, color=$color)
-SUITE["OPF plotting"]["single color"]["large file"] = @benchmarkable plantviz($opf_large, color=$color)
+SUITE["OPF plotting"]["single color"]["small file"] = @benchmarkable plantviz($opf_small, color=$color, cache=$cache)
+SUITE["OPF plotting"]["single color"]["medium file"] = @benchmarkable plantviz($opf_medium, color=$color, cache=$cache)
+SUITE["OPF plotting"]["single color"]["large file"] = @benchmarkable plantviz($opf_large, color=$color, cache=$cache)
 
 # Attribute color:
 color = :mesh_index
@@ -47,7 +48,7 @@ global i = Ref(0)
 transform!(opf_small, :geometry => (x -> i[] += 1) => color, ignore_nothing=true)
 transform!(opf_medium, :geometry => (x -> i[] += 1) => color, ignore_nothing=true)
 transform!(opf_large, :geometry => (x -> i[] += 1) => color, ignore_nothing=true)
-SUITE["OPF plotting"]["attribute color"]["small file"] = @benchmarkable plantviz($opf_small, color=$color)
-SUITE["OPF plotting"]["attribute color"]["medium file"] = @benchmarkable plantviz($opf_medium, color=$color)
-SUITE["OPF plotting"]["attribute color"]["large file"] = @benchmarkable plantviz($opf_large, color=$color)
+SUITE["OPF plotting"]["attribute color"]["small file"] = @benchmarkable plantviz($opf_small, color=$color, cache=$cache)
+SUITE["OPF plotting"]["attribute color"]["medium file"] = @benchmarkable plantviz($opf_medium, color=$color, cache=$cache)
+SUITE["OPF plotting"]["attribute color"]["large file"] = @benchmarkable plantviz($opf_large, color=$color, cache=$cache)
 
