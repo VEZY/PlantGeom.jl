0eaea2905bbf3fbb3173dfcaed7c82bd32ce43df
Rémi Vezy <VEZY@users.noreply.github.com>
2025-09-08 17:12:16 +0200

    Update benchmarks.jl
    force figure display for benchmarking

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -2,6 +2,7 @@ using BenchmarkTools
 using PlantGeom
 using MultiScaleTreeGraph
 using CairoMakie
+using Downloads
 
 # auxiliary variables
 file_small = joinpath(dirname(dirname(pathof(PlantGeom))), "test", "files", "simple_plant.opf")
@@ -24,6 +25,7 @@ SUITE["OPF read"]["large file"] = @benchmarkable read_opf($file_large)
 # ---------
 # Plotting
 # ---------
+plantviz_display(opf; kwargs...) = display(plantviz(opf; kwargs...))
 
 SUITE["OPF plotting"] = BenchmarkGroup()
 
@@ -32,15 +34,15 @@ opf_medium = read_opf(file_medium)
 opf_large = read_opf(file_large)
 
 # Reference mesh:
-SUITE["OPF plotting"]["single color"]["small file"] = @benchmarkable plantviz($opf_small, cache=$cache)
-SUITE["OPF plotting"]["single color"]["medium file"] = @benchmarkable plantviz($opf_medium, cache=$cache)
-SUITE["OPF plotting"]["single color"]["large file"] = @benchmarkable plantviz($opf_large, cache=$cache)
+SUITE["OPF plotting"]["single color"]["small file"] = @benchmarkable plantviz_display($opf_small, cache=$cache)
+SUITE["OPF plotting"]["single color"]["medium file"] = @benchmarkable plantviz_display($opf_medium, cache=$cache)
+SUITE["OPF plotting"]["single color"]["large file"] = @benchmarkable plantviz_display($opf_large, cache=$cache)
 
 # Single color:
 color = :green
-SUITE["OPF plotting"]["single color"]["small file"] = @benchmarkable plantviz($opf_small, color=$color, cache=$cache)
-SUITE["OPF plotting"]["single color"]["medium file"] = @benchmarkable plantviz($opf_medium, color=$color, cache=$cache)
-SUITE["OPF plotting"]["single color"]["large file"] = @benchmarkable plantviz($opf_large, color=$color, cache=$cache)
+SUITE["OPF plotting"]["single color"]["small file"] = @benchmarkable plantviz_display($opf_small, color=$color, cache=$cache)
+SUITE["OPF plotting"]["single color"]["medium file"] = @benchmarkable plantviz_display($opf_medium, color=$color, cache=$cache)
+SUITE["OPF plotting"]["single color"]["large file"] = @benchmarkable plantviz_display($opf_large, color=$color, cache=$cache)
 
 # Attribute color:
 color = :mesh_index
@@ -48,7 +50,6 @@ global i = Ref(0)
 transform!(opf_small, :geometry => (x -> i[] += 1) => color, ignore_nothing=true)
 transform!(opf_medium, :geometry => (x -> i[] += 1) => color, ignore_nothing=true)
 transform!(opf_large, :geometry => (x -> i[] += 1) => color, ignore_nothing=true)
-SUITE["OPF plotting"]["attribute color"]["small file"] = @benchmarkable plantviz($opf_small, color=$color, cache=$cache)
-SUITE["OPF plotting"]["attribute color"]["medium file"] = @benchmarkable plantviz($opf_medium, color=$color, cache=$cache)
-SUITE["OPF plotting"]["attribute color"]["large file"] = @benchmarkable plantviz($opf_large, color=$color, cache=$cache)
-
+SUITE["OPF plotting"]["attribute color"]["small file"] = @benchmarkable plantviz_display($opf_small, color=$color, cache=$cache)
+SUITE["OPF plotting"]["attribute color"]["medium file"] = @benchmarkable plantviz_display($opf_medium, color=$color, cache=$cache)
+SUITE["OPF plotting"]["attribute color"]["large file"] = @benchmarkable plantviz_display($opf_large, color=$color, cache=$cache)
