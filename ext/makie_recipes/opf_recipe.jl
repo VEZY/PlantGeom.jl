44cd810032920cc0861b8d3847c394b839982d86
Rémi Vezy <VEZY@users.noreply.github.com>
2025-09-08 17:51:32 +0200

    Update opf_recipe.jl
    Adding cache argument to PlantViz just for being able to perform benchmarks with #113

--- ext/makie_recipes/opf_recipe.jl
+++ ext/makie_recipes/opf_recipe.jl
@@ -132,6 +132,7 @@ Makie.@recipe PlantViz (mtg,) begin
     "Filter the MTG nodes to be plotted by link"
     link = nothing
     visible = true
+    caching = false
 end
 
 Makie.args_preferred_axis(mtg::MultiScaleTreeGraph.Node) = Makie.LScene
