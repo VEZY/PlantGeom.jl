46ff0f8c1827f786c041edee6b658f9b91fe3381
Documenter.jl <documenter@juliadocs.github.io>
2026-08-28 16:09:09 +0000

    build based on 7f59b40

---
+++ dev/assets/getting_started_makie_3d.md.BJ-EnDO0.lean.js
@@ -0,0 +1,25 @@
+import{_ as t,C as n,ap as l,o as h,c as p,ao as i,E as k,w as o,a2 as r,j as d}from"./chunks/framework.CMQabvLM.js";const c="/PlantGeom.jl/dev/assets/zudjhod.Dm_CDT8T.png",g="/PlantGeom.jl/dev/assets/qqjovjx.BJxCSmbv.png",E="/PlantGeom.jl/dev/assets/wfyckui.CPdf9JCk.png",y="/PlantGeom.jl/dev/assets/abervww.D53fzE4c.png",u="/PlantGeom.jl/dev/assets/jqhuxuz.C4lC3dwD.png",m="/PlantGeom.jl/dev/assets/ekwjfay.DyljxDlc.png",F="/PlantGeom.jl/dev/assets/nwdefzf.ChknX7Ie.png",b="/PlantGeom.jl/dev/assets/gszyhuw.CKfasTbq.png",C="/PlantGeom.jl/dev/assets/gszyhuw.CKfasTbq.png",v="/PlantGeom.jl/dev/assets/coffee_steps.B62IGE-s.mp4",q=JSON.parse('{"title":"3D Plotting with Makie.jl","description":"","frontmatter":{},"headers":[],"relativePath":"getting_started/makie_3d.md","filePath":"getting_started/makie_3d.md","lastUpdated":null}'),f={name:"getting_started/makie_3d.md"},_={class:"vp-raw-html",innerHTML:`<div><div class="bonito-fragment" id="c2966f2d-0ada-4db1-95ca-0a4e81befdd2" data-jscall-id="root"><div><script src="bonito/js/Bonito.bundled7919512299027874894.js" type="module"><\/script><style></style></div><div><script type="module">Bonito.init_session("c2966f2d-0ada-4db1-95ca-0a4e81befdd2", Bonito.fetch_binary('bonito/bin/48ded3964598abc3e38dbe037a299661aefb1d81-4076387809786054186.bin'), "root", false);
+<\/script><div></div></div></div><div class="bonito-fragment" id="98286c07-51fb-40d4-ad5b-0d9725a6a5e2" data-jscall-id="subsession-application-dom"><div><style></style><style>.wglmakie-spinner {
+  left: 50%;
+  z-index: 1000;
+  height: 40px;
+  position: absolute;
+  border-radius: 50%;
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  animation: wglmakie-spin 1.0s linear infinite;
+  top: 50%;
+  border-top: 4px solid currentColor;
+  width: 40px;
+  pointer-events: none;
+  transform: translate(-50%, -50%);
+}
+@keyframes wglmakie-spin {
+  0% {
+    transform: translate(-50%, -50%) rotate(0deg);
+  }
+  100% {
+    transform: translate(-50%, -50%) rotate(360deg);
+  }
+}
+</style></div><div><script type="module">Bonito.init_session("98286c07-51fb-40d4-ad5b-0d9725a6a5e2", Bonito.fetch_binary('bonito/bin/3f0be51e64ed7e1a66ec423ba55d6ad565f3907b-1662454283378556541.bin'), "sub", false);
+<\/script><div style="width: 100%; height: 100%; position: relative;" data-jscall-id="1"><canvas height="450px" data-jp-suppress-context-menu style="display: block" data-jscall-id="2" data-lm-suppress-shortcuts="true" width="600px" tabindex="0"></canvas><div class="wglmakie-spinner" data-jscall-id="3"></div></div></div></div></div>`};function B(x,s,w,D,A,j){const a=n("ClientOnly"),e=l("exec-scripts");return h(),p("div",null,[s[0]||(s[0]=i("",11)),k(a,null,{default:o(()=>[r(d("div",_,null,512),[[e]])]),_:1}),s[1]||(s[1]=i("",67))])}const P=t(f,[["render",B]]);export{q as __pageData,P as default};
